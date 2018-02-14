7541da2f97e1deaa54a4e13c01bc6f510541ed25
joyce-wu <jwu26@stuy.edu>
2018-02-14 13:21:10 -0500

    initial commit

---
+++ queries.py
@@ -0,0 +1,31 @@
+'''
+Joyce Wu & Queenie xiang
+SoftDev2 pd7
+K04 -- Mi only nyam ital food, mon!
+2018-02-14
+'''
+
+from pymongo import MongoClient
+
+c = MongoClient('lisa.stuy.edu')
+mfDB = c.test
+collec = mfDB.restaurants
+
+def borough(boro):
+    restaurants = collec.find({"borough": boro})
+    for r in restaurants:
+        print r
+
+def zip_code(zip):
+    restaurants = collec.find({"address.zipcode": str(zip)})
+    for r in restaurants:
+        print r
+
+def zip_grade(zip, grade):
+    restaurants = collec.find({$and: [{"address.zipcode":str(zip)}, {"grade":grade}]})
+    for r in restaurants:
+        print r
+        
+#borough("Manhattan")
+#zip_code(10025)
+zip_grade(10025, 'A')
