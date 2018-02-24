29bcb4091e334790b9ee01b28e26f90855f7434e
joyce-wu <jwu26@stuy.edu>
2018-02-24 15:10:22 -0500

    need to create functions

--- meteorites.py
+++ meteorites.py
@@ -1,3 +1,17 @@
+'''
+Joyce Wu & Queenie Xiang
+Softdev2 pd7 
+K05 -- Import/Export Bank
+2018-02-26
+'''
+
+'''
+Dataset: Record of Earth Meteorite Landings
+Description: Contains information about location, date, name, mass, and unique id of each meteorite
+Download hyperlink: https://data.nasa.gov/resource/y77d-th95.json
+Import: We opened the url to retrieve data and imported as json. write_json writes the json object into meteorites.json file
+'''
+
 from pymongo import MongoClient
 import urllib2, json
 
@@ -12,7 +26,28 @@ def import_info():
     data = json.loads(resp.read())
     return data
 
+def write_json(data):
+    f = open("meteorites.json", "w+")
+    f.write(str(data))
+    f.close()
 
+#retrieve data and insert into database and imported into json file
 #data = import_info()
+#write_json(data)
 #collie.insert_many(data)
 
+#retrieves all meteorite landings with mass less than n  
+def mass(n):
+
+#retrieves all meteorite landings with that recclass
+def recclass(recclass):
+ 
+#retrieves all meteorite landings with names that start with 'letter'
+#and have a mass less than 'mass' 
+def name_mass(letter, mass):
+
+#retrieves meteorite landings with mass between mass1 and mass2
+def between_masses(mass1, mass2):
+
+#retrieces meteorite landings with year, month, date, range of days)
+def date_range(y, m, d, range): 
